6c824fa14fb2949d1884b26c668cb9a158a6fdb4
FacuAlonso <112171600+FacuAlonso@users.noreply.github.com>
2026-02-22 16:56:10 -0300

    asbv

---
+++ PrimeraParte/src/paquete.js
@@ -0,0 +1,8 @@
+const Paquete = function(montoGBDatos, montoMinutosLlamadas, diasDeDuracion, costoEnPesos){
+    this.montoGBDatos = montoGBDatos;
+    this.montoMinutosLlamadas = montoMinutosLlamadas;
+    this.diasDeDuracion = diasDeDuracion;
+    this.costoEnPesos = costoEnPesos;
+}
+
+module.exports = Paquete
